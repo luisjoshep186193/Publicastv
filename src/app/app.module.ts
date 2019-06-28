38f6bb8037c02089acf7b81df28d12b205095f5a
luislanda <luis.joshep@hotmail.com>
2019-06-28 17:42:48 -0500

    creando las plantillas a utilizar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TvComponent } from './components/tv/tv.component';
+import { PlaylistComponent } from './components/playlist/playlist.component';
+import { ElementComponent } from './components/element/element.component';
+import { CreateComponent } from './components/element/create/create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TvComponent,
+    PlaylistComponent,
+    ElementComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule
